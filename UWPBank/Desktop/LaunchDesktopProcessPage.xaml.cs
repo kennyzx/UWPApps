6310ddb1e429055024308ffd0d432a079b689e04
kennyzx <ikennyzx@gmail.com>
2017-06-08 14:10:25 +0800

    Feature: Launch Desktop Process using Full​Trust​Process​Launcher

--- UWPBank/Desktop/LaunchDesktopProcessPage.xaml.cs
+++ UWPBank/Desktop/LaunchDesktopProcessPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,23 +14,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace UWPBank
+namespace UWPBank.Desktop
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LaunchDesktopProcessPage : Page
     {
-        public MainPage()
+        public LaunchDesktopProcessPage()
         {
             this.InitializeComponent();
         }
-
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        
+        private async void BtnLaunchDesktopProcess_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Desktop.LaunchDesktopProcessPage));
-        }
+            await Full​Trust​Process​Launcher.LaunchFullTrustProcessForAppAsync("App");
+        }        
     }
 }
