eebafcfd250cac44a0c9b14b44938a86b2100392
kennyzx <ikennyzx@gmail.com>
2017-10-16 21:40:06 +0800

    Show the captured picture

--- UWPBank/MediaPage.xaml.cs
+++ UWPBank/MediaPage.xaml.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -53,6 +56,21 @@ namespace UWPBank
         private async void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
         {
             await ReInitCamera();
+        }
+
+        private async void btnTakePicture_Click(object sender, RoutedEventArgs e)
+        {
+            var captureStream = new InMemoryRandomAccessStream();
+            {
+                await mediaCapture.CapturePhotoToStreamAsync(
+                    Windows.Media.MediaProperties.ImageEncodingProperties.CreateJpeg(),
+                     captureStream);
+                
+                BitmapImage bitmap = new BitmapImage();
+                captureStream.Seek(0);
+                bitmap.SetSource(captureStream);
+                previewImage.Source = bitmap;
+            }
         }
     }
 }
