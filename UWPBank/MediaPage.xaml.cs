37786a7696c25fd3f24c0ffa4360758bb4b87416
kennyzx <ikennyzx@gmail.com>
2018-04-15 11:45:26 +0800

    Handle UnauthorizedAccessException if user doesnot grant permission

--- UWPBank/MediaPage.xaml.cs
+++ UWPBank/MediaPage.xaml.cs
@@ -16,6 +16,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,39 +29,65 @@ namespace UWPBank
     public sealed partial class MediaPage : Page
     {
         MediaCapture mediaCapture;
+        bool isInitializedSuccessfully;
+        
         public MediaPage()
         {
             this.InitializeComponent();
         }
 
-        private async System.Threading.Tasks.Task ReInitCamera()
+        private async Task CreateOrReInitCamera()
         {
+            if (mediaCapture != null)
+                mediaCapture.Dispose();
+
+            isInitializedSuccessfully = false;
             mediaCapture = new MediaCapture();
-            await mediaCapture.InitializeAsync();
-            previewElement.Source = mediaCapture;
-            await mediaCapture.StartPreviewAsync();
+            try
+            {
+                await mediaCapture.InitializeAsync();
+                previewElement.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isInitializedSuccessfully = true;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                await new MessageDialog(ex.Message, "Cannot use camera").ShowAsync();
+                //TODO: Redirect user to Settings to grant permission to the app
+            }
+        }
+
+        private async Task DisposeCamera()
+        {
+            if (isInitializedSuccessfully)
+                await mediaCapture.StopPreviewAsync();
+
+            mediaCapture.Dispose();
+            mediaCapture = null;
         }
 
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            await ReInitCamera();
+            await CreateOrReInitCamera();
             Window.Current.Activated += Current_Activated;
         }
 
         protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
-            await mediaCapture.StopPreviewAsync();
-            mediaCapture = null;
+            await DisposeCamera();
             Window.Current.Activated -= Current_Activated;
         }
 
         private async void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
         {
-            await ReInitCamera();
+            await CreateOrReInitCamera();
         }
 
         private async void btnTakePicture_Click(object sender, RoutedEventArgs e)
         {
+            if (!isInitializedSuccessfully)            
+                return;
+
             var captureStream = new InMemoryRandomAccessStream();
             {
                 await mediaCapture.CapturePhotoToStreamAsync(
